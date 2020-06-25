0be7ebc3a9913a87984df1fe0dc6bdae3bdb448a
Valeriy <62266921+MaxSMoke-Official@users.noreply.github.com>
2020-06-25 15:38:00 +1000

    Дописал keygen.h

--- keygen.h
+++ keygen.h
@@ -6,45 +6,42 @@
 #include <iostream>
 #include <cstdlib>
 #include "modules.h"
-using namespace std;
 void KeyGen () {
-int a,b;
-cout << "====================================" << endl;
-go:
-cout << "Choice difficulty: 4, 6 or 8" << endl;
-cout << "0-Exit" << endl;
-cout << "====================================" << endl;
-cin >> a;
-if (a == 4) {
-four ();
-goto go;
-} else if (a == 6) {
-six();
-goto go;
-} else if (a == 8) {
-eight();
-goto go;
-} else if (a == 0) {
-cout << "Exit to menu? 1)Yes, 2)No" << endl;
-cout << "====================================" << endl;
-cin >> b;
-if (b == 1) {						
-goto end;
-} else if (b == 2){
-goto go;
-} else {
-cout << "Error 301" << endl;
-cout << "====================================" << endl;
-cin >> b;
-}
-goto go;
-} else {
-cout << "Error 302" << endl;
-cout << "====================================" << endl;
-cin >> b;
-goto go;
-end:
-cin >> b;
-}
+ int a,b;
+ std::cout << "====================================\n";
+ go:
+ cout << "Choice difficulty: 4, 6 or 8" << endl;
+ std::cout << "0-Exit\n";
+ std::cout << "====================================\n";
+ getch(std::cin, a);
+ if (a == "4") {
+  four ();
+  goto go;
+ } else if (a == "6") {
+  six();
+  goto go;
+ } else if (a == "8") {
+  eight();
+  goto go;
+ } else if (a == "e" || a == "E") {
+  std::cout << "Exit to menu? 1)Yes, 2)No\n";
+  std::cout << "====================================\n";
+  getline(std::cin, b)
+  if (b == "1") {						
+   goto end;
+  } else if (b == "2"){
+   goto go;
+  } else {
+   std::cout << "Error 301\n";
+   std::cout << "====================================\n";
+  }
+  goto go;
+ } else {
+ std::cout << "Error 302\n";
+ std::cout << "====================================\n";
+ goto go;
+ end:
+ return;
+ }
 }
 #endif
