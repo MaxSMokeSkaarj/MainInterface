25266c62975d075e4f3b0f77532610bdced4f72f
root <root@localhost>
2020-06-30 13:51:38 +1000

    перенес математику в modules.h

--- calc.h
+++ calc.h
@@ -5,88 +5,10 @@
 #include <sstream>
 #include <string>
 #include <cmath>
-
-void MathCore(std::string arr[]) {
- std::string a = arr[0];
- std::string b = arr[1];
- std::string c = arr[2];
- if(b == "+"){
-  std::stringstream ssa(a);
-  std::stringstream ssc(c);
-  double da = 0.0;
-  double dc = 0.0;
-  ssa >> da;
-  ssc >> dc;
-  std::cout << a + " + " + c + " = " << da + dc << std::endl;
- } else if(b == "-"){
-  std::stringstream ssa(a);
-  std::stringstream ssc(c);
-  double da = 0.0;
-  double dc = 0.0;
-  ssa >> da;
-  ssc >> dc;
-  std::cout << a + " - " + c + " = " << da - dc << std::endl;
- } else if(b == "*"){
-  std::stringstream ssa(a);
-  std::stringstream ssc(c);
-  double da = 0.0;
-  double dc = 0.0;
-  ssa >> da;
-  ssc >> dc;
-  std::cout << a + " * " + c + " = " << da * dc << std::endl;
- } else if(b == "/"){
-  std::stringstream ssa(a);
-  std::stringstream ssc(c);
-  double da = 0.0;
-  double dc = 0.0;
-  ssa >> da;
-  ssc >> dc;
-  std::cout << a + " / " + c + " = " << da / dc << std::endl;
- } else if(a == "sin"){
-  std::stringstream ssb(b);
-  double db = 0.0;
-  ssb >> db;
-  std::cout << "Sinus " + b + " = " << sin(db) << std::endl;
- } else if(a == "cos"){
-  std::stringstream ssb(b);
-  double db = 0.0;
-  ssb >> db;
-  std::cout << "Cosinus " + b + " = " << cos(db) << std::endl;
- } else if(a == "tan"){
-  std::stringstream ssb(b);
-  double db = 0.0;
-  ssb >> db;
-  std::cout << "Tangent " + b + " = " << tan(db) << std::endl;
- } else if(a == "log"){
-  std::stringstream ssb(b);
-  double db = 0.0;
-  ssb >> db;
-  std::cout << "Logarithm " + b + " = " << log(db) << std::endl;
- } else {
- std::cout << "Error 102\n";
- };
-};
-
-void stringProcessor(std::string line)
-{
- std::string s = line;
- std::istringstream iss(s);
- int i = 0;
- std::string arr[4];
- do {
-  std::string subs;
-  iss >> subs;
-  arr[i]= subs;
-  i++;
-  //std:: cout << "Substring: " << arr[i] << std::endl;
- } while (iss);
- MathCore(arr);
-};
+#include "modules.h"
 
 void calc() {
- std::cout << "====================================\n";
  std::cout << "MainInterface v1.6 by KillSZ\n";
- std::cout << "====================================\n";
  back:
  std::cout << "Input a mathematical example...\nType 'e' to exit\n";
  std::string in;
