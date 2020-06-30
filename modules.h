e0905bcaaf1afa4b1b4d32b2b89358264fd9434e
root <root@localhost>
2020-06-30 13:52:30 +1000

    обновление модулей для калькулятора

--- modules.h
+++ modules.h
@@ -7,123 +7,83 @@ structure*/
 #include <cstdlib>
 using namespace std;
 //calculator
-void sum () {
-double a,b,c;
-cout << "Enter first number" << endl;
-cout << "====================================" << endl;
-cin >> a;
-cout << "Enter second number" << endl;
-cout << "====================================" << endl;
-cin >> b;
-c = a + b;
-cout << "Summ: " << c << endl;
-cout << "====================================" << endl;
-}
-void min () {
-double a,b,c,d;
-cout << "Enter first number" << endl;
-cout << "====================================" << endl;
-cin >> a;
-cout << "Enter second number" << endl;
-cout << "====================================" << endl;
-cin >> b;
-c = b - a;
-d = a - b;
-cout << "Second munus first: " << c << endl;
-cout << "====================================" << endl;
-cout << "First minus second: " << d << endl;
-cout << "====================================" << endl;
-}
-void multiply () {
-double a,b,c;
-cout << "Enter first number" << endl;
-cout << "====================================" << endl;
-cin >> a;
-cout << "Enter Second number" << endl;
-cout << "====================================" << endl;
-cin >> b;
-c = a * b;
-cout << "Multyply: " << c << endl;
-cout << "====================================" << endl;
-}
-void divide () {
-double a,b,c,d;
-cout << "Enter first number" << endl;
-cout << "====================================" << endl;
-cin >> a;
-cout << "Enter second number" << endl;
-cout << "====================================" << endl;
-cin >> b;
-c = b / a;
-d = a / b;
-cout << "Second divide first: " << c << endl;
-cout << "====================================" << endl;
-cout << "First divide second: " << d << endl;
-cout << "====================================" << endl;
-}
-void square () {
-double a,b;
-cout << "Enter number" << endl;
-cout << "====================================" << endl;
-cin >> a;
-b = a * a;
-cout << "Square: " << b << endl;
-cout << "====================================" << endl;
-}
-void cube () {
-double a,b;
-cout << "Enter number:" << endl;
-cout << "====================================" << endl;
-cin >> a;
-b = a * a * a;
-cout << "Cube: " << b << endl;
-cout << "====================================" << endl;
-}
-void root () {
-double a,b;
-cout << "Enter number" << endl;
-cout << "====================================" << endl;
-cin >> a;
-b = sqrt(a);
-cout << "Root: " << b << endl;
-cout << "====================================" << endl;
-}
-void sin () {
-double a,b;
-cout << "Enter number" << endl;
-cout << "====================================" << endl;
-cin >> a;
-b=sin(a);
-cout << "Sinus: " << b << endl;
-cout << "====================================" << endl;
-}
-void cos () {
-double a,b;
-cout << "Enter number" << endl;
-cout << "====================================" << endl;
-cin >> a;
-b = cos(a);
-cout << "Cosinus: " << b << endl;
-cout << "====================================" << endl;
-}
-void tan () {
-double a,b;
-cout << "Enter number" << endl;
-cout << "====================================" << endl;
-cin >> a;
-b = tan(a);
-cout << "Tangens: " << a << endl;
-cout << "====================================" << endl;
-}
-void log () {
-double a,b;
-cout << "Enter number" << endl;
-cout << "====================================" << endl;
-cin >> a;
-b = log(a);
-cout << "Logarifm: " << b << endl;
-cout << "====================================" << endl;
-}
+void MathCore(std::string arr[]) {
+ std::string a = arr[0];
+ std::string b = arr[1];
+ std::string c = arr[2];
+ if(b == "+"){
+  std::stringstream ssa(a);
+  std::stringstream ssc(c);
+  double da = 0.0;
+  double dc = 0.0;
+  ssa >> da;
+  ssc >> dc;
+  std::cout << a + " + " + c + " = " << da + dc << std::endl;
+ } else if(b == "-"){
+  std::stringstream ssa(a);
+  std::stringstream ssc(c);
+  double da = 0.0;
+  double dc = 0.0;
+  ssa >> da;
+  ssc >> dc;
+  std::cout << a + " - " + c + " = " << da - dc << std::endl;
+ } else if(b == "*"){
+  std::stringstream ssa(a);
+  std::stringstream ssc(c);
+  double da = 0.0;
+  double dc = 0.0;
+  ssa >> da;
+  ssc >> dc;
+  std::cout << a + " * " + c + " = " << da * dc << std::endl;
+ } else if(b == "/"){
+  std::stringstream ssa(a);
+  std::stringstream ssc(c);
+  double da = 0.0;
+  double dc = 0.0;
+  ssa >> da;
+  ssc >> dc;
+  std::cout << a + " / " + c + " = " << da / dc << std::endl;
+ } else if(a == "sin"){
+  std::stringstream ssb(b);
+  double db = 0.0;
+  ssb >> db;
+  std::cout << "Sinus " + b + " = " << sin(db) << std::endl;
+ } else if(a == "cos"){
+  std::stringstream ssb(b);
+  double db = 0.0;
+  ssb >> db;
+  std::cout << "Cosinus " + b + " = " << cos(db) << std::endl;
+ } else if(a == "tan"){
+  std::stringstream ssb(b);
+  double db = 0.0;
+  ssb >> db;
+  std::cout << "Tangent " + b + " = " << tan(db) << std::endl;
+ } else if(a == "log"){
+  std::stringstream ssb(b);
+  double db = 0.0;
+  ssb >> db;
+  std::cout << "Logarithm " + b + " = " << log(db) << std::endl;
+ } else {
+ std::cout << "Error 102\n";
+ };
+};
+
+void stringProcessor(std::string line)
+{
+ std::string s = line;
+ std::istringstream iss(s);
+ int i = 0;
+ std::string arr[4];
+ do {
+  std::string subs;
+  iss >> subs;
+  arr[i]= subs;
+  i++;
+  //std:: cout << "Substring: " << arr[i] << std::endl;
+ } while (iss);
+ MathCore(arr);
+};
+
 //keygen
 void four () {
 cout << "4-numberic code: " << 1000 + rand() % 1000 << endl;
