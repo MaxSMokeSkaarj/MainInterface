86d290ef7277edb515e7738fd9039d96fa6e896e
Valeriy <62266921+MaxSMoke-Official@users.noreply.github.com>
2020-07-05 22:23:48 +1000

    Навел марафет(отступы из пробелов)

--- main.cpp
+++ main.cpp
@@ -4,32 +4,33 @@
 #include "calc.h"
 #include "keygen.h"
 int main() {
-	std::string a, b;
-	std::cout << "MainInterface v1.6 by MaxSMoke\n";
-	again:
-	std::cout << "Choose:\n1)Calculator\n2)KeyGen\n3)Converter\nType 'e' to exit\n";
-	getline(std::cin, a);
-	if (a == "1") {
-		calc ();
-	} else if (a == "2") {
-		KeyGen();
-	} else if (a == "3") {
-		std::cout << "Not work...\n";
-		//shell();
-	} else if (a == "E" || a == "e") {
-		std::cout << "Exit? y/N\n";
-		getline(std::cin, b);
-		if (b == "Y" || b == "y") {
-			goto end;
-		} else if (b == "n" || b == "N") {
-			goto again;
-		} else {
-			std::cout << "Error 101\n";
-		}
-	} else {
-		std::cout << "Error 102\n";
-	}
-	goto again;
-	end:
-	return 0;
+    std::string a,
+    b;
+    std::cout << "MainInterface v1.6 by MaxSMoke\n";
+    again:
+    std::cout << "Choose:\n1)Calculator\n2)KeyGen\n3)Converter\nType 'e' to exit\n";
+    getline(std::cin, a);
+    if (a == "1") {
+        calc ();
+    } else if (a == "2") {
+        KeyGen();
+    } else if (a == "3") {
+        std::cout << "Not work...\n";
+        //shell();
+    } else if (a == "E" || a == "e") {
+        std::cout << "Exit? y/N\n";
+        getline(std::cin, b);
+        if (b == "Y" || b == "y") {
+            goto end;
+        } else if (b == "n" || b == "N") {
+            goto again;
+        } else {
+            std::cout << "Error 101\n";
+        }
+    } else {
+        std::cout << "Error 102\n";
+    }
+    goto again;
+    end:
+    return 0;
 }
