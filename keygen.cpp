28d2cf38c225e760f2c6af581ec14021b9945214
Valeriy <62266921+MaxSMoke-Official@users.noreply.github.com>
2021-08-18 22:18:55 +1000

    update keygen.cpp

--- keygen.cpp
+++ keygen.cpp
@@ -2,23 +2,29 @@
 #include <cstdlib>
 
 int KeyGen() {
-  std::string a,b;
+
+  //pre
+  four();
+  six();
+  eight();
+
   while (true) {
+    std::string choice, exitChoice;
     std::cout << "Choice difficulty: 4, 6 or 8\n";
     std::cout << "Type 'e' to exit\n";
-    getline(std::cin, a);
-    if (a == "4") {
+    getline(std::cin, choice);
+    if (choice == "4") {
       four ();
-    } else if (a == "6") {
+    } else if (choice == "6") {
       six();
-    } else if (a == "8") {
+    } else if (choice == "8") {
       eight();
-    } else if (a == "e" || a == "E") {
+    } else if (choice == "e" || choice == "E") {
       std::cout << "Exit to menu? y/N\n";
-      getline(std::cin, b);
-      if (b == "y" || b == "Y") {
+      getline(std::cin, exitChoice);
+      if (exitChoice == "y" || exitChoice == "Y") {
         return 0;
-      } else if (b == "n" || b == "N") {
+      } else if (exitChoice == "n" || exitChoice == "N") {
         std::cout << "Aborting...\n";
       } else {
         std::cout << "Error 301\n";
