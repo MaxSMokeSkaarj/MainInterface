ced37af3d8fa65a56426e49ebe257d95b45a742b
Valeriy <62266921+MaxSMoke-Official@users.noreply.github.com>
2021-08-18 20:27:38 +1000

    Смена типа функции

--- keygen.cpp
+++ keygen.cpp
@@ -3,39 +3,33 @@
 #include <iostream>
 #include <cstdlib>
 
-void KeyGen () {
-    std::string a,
-    b;
-    go:
+int KeyGen () {
+  std::string a,
+  b;
+  while true () {
     std::cout << "Choice difficulty: 4, 6 or 8\n";
     std::cout << "Type 'e' to exit\n";
     getline(std::cin, a);
     if (a == "4") {
-        four ();
-        goto go;
+      four ();
     } else if (a == "6") {
-        six();
-        goto go;
+      six();
     } else if (a == "8") {
-        eight();
-        goto go;
+      eight();
     } else if (a == "e" || a == "E") {
-        std::cout << "Exit to menu? y/N\n";
-        getline(std::cin, b);
-        if (b == "y" || b == "Y") {
-            goto end;
-        } else if (b == "n" || b == "N") {
-            goto go;
-        } else {
-            std::cout << "Error 301\n";
-        }
-        goto go;
+      std::cout << "Exit to menu? y/N\n";
+      getline(std::cin, b);
+      if (b == "y" || b == "Y") {
+        return 0;
+      } else if (b == "n" || b == "N") {
+        //nothing to do
+      } else {
+        std::cout << "Error 301\n";
+      }
     } else {
-        std::cout << "Error 302\n";
-        goto go;
-        end:
-        return;
+      std::cout << "Error 302\n";
     }
+  }
 }
 
 #endif
