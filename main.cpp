a12a6e189b47b72cbf6e10b5a7d77313129fbef6
Valeriy <62266921+MaxSMoke-Official@users.noreply.github.com>
2021-08-18 19:44:17 +1000

    Убираю goto

--- main.cpp
+++ main.cpp
@@ -6,31 +6,32 @@
 #include "conv.h"
 
 int main() {
-  std::string a,
-  b;
-  std::cout << "MainInterface v1.6 by MaxSMoke\n";
-  again:
-  std::cout << "Choose:\n1)Calculator\n2)KeyGen\n3)Converter\nType 'e' to exit\n";
-  getline(std::cin, a);
-  if (a == "1") {
-    calc ();
-  } else if (a == "2") {
-    KeyGen();
-  } else if (a == "3") {
-    conv();
-  } else if (a == "E" || a == "e") {
-    std::cout << "Exit? y/N\n";
-    getline(std::cin, b);
-    if (b == "Y" || b == "y") {
-      return 0;
-    } else if (b == "n" || b == "N") {
-      goto again;
+  while(true) {
+    std::string a,
+    b;
+    std::cout << "MainInterface v1.6 by MaxSMoke\n";
+    again:
+    std::cout << "Choose:\n1)Calculator\n2)KeyGen\n3)Converter\nType 'e' to exit\n";
+    getline(std::cin, a);
+    if (a == "1") {
+      calc ();
+    } else if (a == "2") {
+      KeyGen();
+    } else if (a == "3") {
+      conv();
+    } else if (a == "E" || a == "e") {
+      std::cout << "Exit? y/N\n";
+      getline(std::cin, b);
+      if (b == "Y" || b == "y") {
+        return 0;
+      } else if (b == "n" || b == "N") {
+        goto again;
+      } else {
+        std::cout << "Error 101\n";
+      }
     } else {
-      std::cout << "Error 101\n";
+      std::cout << "Error 102\n";
     }
-  } else {
-    std::cout << "Error 102\n";
   }
-  goto again;
   return 0;
 }
