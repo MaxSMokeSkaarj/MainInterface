065a35ec4f6a1825cdb97c92cfc07e1f4b02e54b
Valeriy <62266921+MaxSMoke-Official@users.noreply.github.com>
2021-08-18 21:39:38 +1000

    update calc.cpp

--- calc.cpp
+++ calc.cpp
@@ -5,24 +5,24 @@
 
 int calc() {
   while (true) {
-  std::cout << "MainInterface v1.6 by KillSZ\n";
-  std::cout << "Input a mathematical example...\nType 'e' to exit\n";
-  std::string in;
-  getline(std::cin, in);
-  if(in == "E" || in == "e") {
-    std::cout << "Exit? y/N\n";
-    std::string chc;
-    getline(std::cin, chc);
-    if (chc == "y" || chc == "Y") {
-      return 0;
-    } else if (chc == "N" || chc == "n") {
-      std::cout << "Aborting...\n";
-    } else {
-      std::cout << "Error 201\n";
-    }
-  };
+    std::cout << "MainInterface v1.6 by KillSZ\n";
+    std::cout << "Input a mathematical example...\nType 'e' to exit\n";
+    std::string in;
+    getline(std::cin, in);
+    if(in == "E" || in == "e") {
+      std::cout << "Exit? y/N\n";
+      std::string chc;
+      getline(std::cin, chc);
+      if (chc == "y" || chc == "Y") {
+        return 0;
+      } else if (chc == "N" || chc == "n") {
+        std::cout << "Aborting...\n";
+      } else {
+        std::cout << "Error 201\n";
+      }
+    };
 
-  stringProcessor(in);
+    stringProcessor(in);
   }
   return 0;
 }
